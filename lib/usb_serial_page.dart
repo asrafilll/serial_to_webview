0d0052ddcaff1eba760d17e8ef6a267515cf2f01
asrafilll <asrafilr@gmail.com>
2023-01-13 08:48:08 +0700

    Fix Parameters

--- lib/usb_serial_page.dart
+++ lib/usb_serial_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:usb_serial/usb_serial.dart';
 
-class TesPage extends StatelessWidget {
+class TesPage extends StatefulWidget {
   const TesPage({super.key});
+
+  @override
+  State<TesPage> createState() => _TesPageState();
+}
+
+class _TesPageState extends State<TesPage> {
+  String _currentStatus = 'Not Connected';
 
   @override
   Widget build(BuildContext context) {
@@ -11,33 +18,40 @@ class TesPage extends StatelessWidget {
         title: const Text('tes'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Column(
-            children: const [
-              Text('Check Ports'),
-            ],
-          ),
-          onPressed: () async {
-            List<UsbDevice> devices = await UsbSerial.listDevices();
-            if (devices.isEmpty) {
-              const Text('No Devices');
-            }
-            if (devices.isNotEmpty) {
-              Text('Devices are detected : $devices');
-            }
-            UsbPort? port;
-            port = await devices[0].create();
-            bool openResult = await port!.open();
-            if (!openResult) {
-              const Text('Failed to Open Port');
-              return;
-            }
-            await port.setDTR(true);
-            await port.setRTS(true);
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Check Ports'),
 
-          // Get the usb serial data by read
-          // Then pass the data to widget WebView_Page
+              onPressed: () async {
+                List<UsbDevice> devices = await UsbSerial.listDevices();
+                if (devices.isEmpty) {
+                  setState(() {
+                    _currentStatus = 'Devices is Empty';
+                  });
+                }
+                if (devices.isNotEmpty) {
+                  setState(() {
+                    _currentStatus = 'Devices are detected : $devices';
+                  });
+                }
+                // UsbPort? port;
+                // port = await devices[0].create();
+                // bool openResult = await port!.open();
+                // if (!openResult) {
+                //   const Text('Failed to Open Port');
+                //   return;
+                // }
+                // await port.setDTR(true);
+                // await port.setRTS(true);
+              },
+
+              // Get the usb serial data by read
+              // Then pass the data to widget WebView_Page
+            ),
+            Text(_currentStatus),
+          ],
         ),
       ),
     );
